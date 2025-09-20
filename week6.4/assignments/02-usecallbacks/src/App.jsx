c120f9dbcec6df0d4bec250db5693ada874ebf30
Biswajit Sarkar <biswajitsarkar0485@gmail.com>
2025-09-20 17:09:16 +0530

    week6.4

---
+++ week6.4/assignments/02-usecallbacks/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Assignment1 } from './components/Assigment1'
+ 
+import { Assignment2 } from './components/Assignment2'
+
+function App() {
+  return (
+    <>
+       {/* <Assignment1/> */}
+      <Assignment2 />
+    </>
+  )
+}
+
+export default App
