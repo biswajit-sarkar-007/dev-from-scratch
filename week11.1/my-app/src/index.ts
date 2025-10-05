a43b125f0261c2e99eb73f39edaef2c091e14296
Biswajit Sarkar <biswajitsarkar0485@gmail.com>
2025-10-05 09:01:43 +0530

    week11.1 some minor changes

--- week11.1/my-app/src/index.ts
+++ week11.1/my-app/src/index.ts
@@ -1,25 +1,22 @@
-// in here we did not used express in http server we only used wrangler for http server 
+// in here we did not used express in http server we only used wrangler for http server
 
 // app.get("/", ()=>{})
 // app.listent(3000)
 
-
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		 return Response.json({
-			msg:"happy new year 2026"
-		 })
+		return new Response('happy new year 2026', {
+			status: 200,
+			headers: { 'Content-Type': 'text/plain' },
+		});
 	},
 } satisfies ExportedHandler<Env>;
- 
 
 // export default {
 // 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 // 		console.log(request.body);
 // 		console.log(request.headers);
 
-		 
-		
 // 		if (request.method === "GET") {
 // 			return Response.json({
 // 				message: "you sent a get request"
