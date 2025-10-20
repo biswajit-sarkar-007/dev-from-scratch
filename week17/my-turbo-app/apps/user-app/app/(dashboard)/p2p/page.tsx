ed9d4a01b0fc445d62d7b5753f6d440f1c09a2a4
Biswajit Sarkar <biswajitsarkar0485@gmail.com>
2025-10-20 23:03:23 +0530

    p2p transfer added

---
+++ week17/my-turbo-app/apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,11 @@
+import { SendCard } from "../../../components/SendCard"
+
+export default () =>{
+    return(
+        <div className="w-full">
+        
+        <SendCard/>
+        
+        </div>
+    )
+}
